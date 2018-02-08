1dc01d6a08889e96af28b46e14902728c860dd07
Manish-a <35912310+Manish-a@users.noreply.github.com>
2018-02-08 09:45:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo"
+<html>
+	<head>
+		<title>16MCA1022</title>
+		<style>
+			body{background-color:black}
+			.text{color:white;text-align:center;left: 0;line-height: 200px;margin: auto;margin-top: -100px;position: absolute;top: 50%;width: 100%;}
+		</style>
+	</head>
+	<body>
+		<h1 class='text'>WELCOME Manisha (16MCA1022) !!!</h1>
+	</body>
+</html>";
+?>
